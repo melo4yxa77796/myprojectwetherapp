15c69f2401d29ae07a3d80a5e607d5fa3b2dc530
yuliia <109953065+melo4yxa77796@users.noreply.github.com>
2022-08-12 16:56:38 +0200

    update forecast api

--- src/index.js
+++ src/index.js
@@ -111,12 +111,12 @@ days.forEach(function(day){
 
     
   }
-function getForecast(coordinates){
-  console.log(coordinates);
-  let apiKey="3fc4a1a542593e4089e587a81b28f31f";
-  let apiUrl=`https://api.openweathermap.org/data/3.0/onecall?lat=${cordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
- axios.get(apiUrl).then(displayForecast);
-}
+  function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "3fc4a1a542593e4089e587a81b28f31f";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+  }
 
   function search(event) {
     event.preventDefault();
